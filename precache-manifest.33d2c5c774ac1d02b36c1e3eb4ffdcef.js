0f244a946d161c4df387ec5fcfd9a2c20548a358
longnv99 <litlong99@gmail.com>
2021-12-23 21:54:56 +0700

    Updates

---
+++ precache-manifest.33d2c5c774ac1d02b36c1e3eb4ffdcef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "224f75b1af774c43f646",
+    "url": "./static/css/main.30fdd1c6.chunk.css"
+  },
+  {
+    "revision": "224f75b1af774c43f646",
+    "url": "./static/js/main.224f75b1.chunk.js"
+  },
+  {
+    "revision": "1a98843cc399bb88051a",
+    "url": "./static/js/1.1a98843c.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9bd5e67c9264c51caaaa872e2f7b64d6",
+    "url": "./index.html"
+  }
+];
